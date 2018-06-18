262304e635ce56e78fb89a0f651a0bec965321af
iulisirbu <37597080+iulisirbu@users.noreply.github.com>
2018-06-18 23:50:37 +0300

    eod 2

--- Array.java
+++ Array.java
@@ -2,15 +2,36 @@ package com.testacademy;
 
 import java.util.List;
 import java.util.ArrayList;
+import static com.testacademy.BubbleSort.*;
+
 
 public class Array {
 
-    private static int sumArrays(int[] arr1){
+    public static int sumArrays(int[] arr1){
         int sum = 0;
         for(int i=0;i<arr1.length;i++){
             sum = sum + arr1[1];
         }
         return sum;
+    }
+
+    public static boolean searchArray(int[] arr, int s){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==s)
+                return true;
+        }
+        return false;
+    }
+
+    public static int minArray(int[] arr) {
+        bubbleSortFor(arr);
+        return arr[0];
+    }
+
+    public static int maxArray(int[] arr) {
+        bubbleSortFor(arr);
+        return arr[arr.length-1];
+
     }
 
     public static void findDistinct(int[] arr1,int[] arr2){
@@ -31,8 +52,12 @@ public class Array {
                 contains = false;
             }
         }
-        for (int var : auxArr) {
-            System.out.print(var + " ");
+        for (int i=0;i<auxArr.size();i++) {
+            System.out.print(auxArr.get(i) + " ");
+             }
+        System.out.print("\nPe pozitiile : ");
+        for (int i=0;i<auxArr.size();i++) {
+            System.out.print(i + " ");
         }
     }
 }
