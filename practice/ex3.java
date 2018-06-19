59f5f6edeacb61e9e08fef8acdc42fd6128e025b
iulisirbu <37597080+iulisirbu@users.noreply.github.com>
2018-06-19 23:25:06 +0300

    eod 2

---
+++ practice/ex3.java
@@ -0,0 +1,15 @@
+package com.testacademy.practice;
+
+public class ex3 {
+
+    public static void main(String[] args) {
+
+        /* 3.Să se calculeze valoare constantei PI, pe baza formulei:    pi/4=1-1/3+1/5-1/7+1/9-... Se vor lua N=3000 de termini în această sumă.*/
+
+        final int N = 3000;
+        double pi = 0;
+
+
+
+    }
+}
