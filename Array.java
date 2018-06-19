5a1fa82c86b08a1bc488e59171167daa76784e55
iulisirbu <37597080+iulisirbu@users.noreply.github.com>
2018-06-19 11:12:50 +0300

    Update Array.java

--- Array.java
+++ Array.java
@@ -16,19 +16,20 @@ public class Array {
     *
     * 18/06/2016
     */
-   public static int sumArrays(int[] arr1){
+   
+    public static int sumArrays(int[] arr1) {
         /* Write a Java program to sum values of an array */
         int sum = 0;
-        for(int i=0;i<arr1.length;i++){
+        for (int i = 0; i < arr1.length; i++) {
             sum = sum + arr1[1];
         }
         return sum;
     }
 
-    public static boolean searchArray(int[] arr, int s){
+    public static boolean searchArray(int[] arr, int s) {
         /* Write a Java program to test if an array contains a specific value. */
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==s)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == s)
                 return true;
         }
         return false;
@@ -43,60 +44,71 @@ public class Array {
     public static int maxArray(int[] arr) {
         /* Write a Java program to find the maximum value of an array.  */
         bubbleSortFor(arr);
-        return arr[arr.length-1];
+        return arr[arr.length - 1];
 
     }
 
-    public static void findDistinct(int[] arr1,int[] arr2){
+    public static void findDistinct(int[] arr1, int[] arr2) {
         /* Write a Java program to find on which index elements of tow arrays are different. */
         boolean contains = false;
         List<Integer> auxArr = new ArrayList<Integer>();
         List<String> index = new ArrayList<String>();
 
-        int[] sortedFirst = bubbleSortFor(arr1);
+        /*int[] sortedFirst = bubbleSortFor(arr1);
         int[] sortedSecond = bubbleSortFor(arr2);
 
-        index.add("First String");
         for(int i=0;i<arr1.length;i++){
-            for (int j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]) {
+            System.out.println(arr1[i]);
+        }
+*/
+        index.add("First String");
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
                     contains = true;
                     break;
                 }
             }
-            if(contains!=true){
+            if (contains != true) {
                 auxArr.add(arr1[i]);
                 index.add(String.valueOf(i));
 
-            }
-            else{
+            } else {
                 contains = false;
             }
         }
         index.add("Second String");
-        for(int i=0;i<arr2.length;i++){
-            for (int j=0;j<arr1.length;j++){
-                if(arr2[i]==arr1[j]) {
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                if (arr2[i] == arr1[j]) {
                     contains = true;
                     break;
                 }
 
             }
-            if(contains!=true){
+            if (contains != true) {
                 auxArr.add(arr2[i]);
 
                 index.add(String.valueOf(i));
 
-            }
-            else{
+            } else {
                 contains = false;
             }
         }
-        for (int i=0;i<auxArr.size();i++) {
+        for (int i = 0; i < auxArr.size(); i++) {
             System.out.print(auxArr.get(i) + " ");
         }
 
         System.out.print("\nPe pozitiile : " + index);
     }
+
+   /* public static void main(String[] args) {
+        int[] array1 = {5,2,8,4,6};
+        int[] array2 = {3,4,1};
+
+        findDistinct(array1,array2);
+
+    }*/
+
 }
 
